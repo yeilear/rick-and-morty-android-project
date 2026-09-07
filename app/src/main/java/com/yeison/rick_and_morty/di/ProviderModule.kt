90865d2d49961703d7cb0ecb2d8128a7cbd16c9a
yeilear <yeisonsanchez25@gmail.com>
2026-09-07 18:39:14 -0500

    Hilt implementation
    - Add provider module and application
    - Add navigation library

---
+++ app/src/main/java/com/yeison/rick_and_morty/di/ProviderModule.kt
@@ -0,0 +1,44 @@
+package com.yeison.rick_and_morty.di
+
+import com.yeison.core.network.dispatcher.DispatcherProvider
+import com.yeison.core.network.dispatcher.DispatcherProviderImpl
+import com.yeison.core.utils.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ProviderModule {
+
+    @Provides
+    @Singleton
+    fun provideDispatcherProvider(): DispatcherProvider = DispatcherProviderImpl()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
